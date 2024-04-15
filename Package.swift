767fff17daa44563916160fee0b021520de5e6be
Seed Mac Developer <developer@seed-design.co.jp>
2024-04-15 17:48:41 +0900

    Using public repository for "PipeModelMac".

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(path: "../PipeModelMac")
+		.package(url: "https://github.com/seed-industrial-designing/PipeModelMac.git", from: "1.0.0"),
 	],
 	targets: [
 		.target(
