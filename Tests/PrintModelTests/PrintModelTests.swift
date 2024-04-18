fb13e608b7ca8f5be4bd0193a82b37918cb0153f
Seed Mac Developer <developer@seed-design.co.jp>
2024-04-18 15:10:13 +0900

    Added `testFourCharString()`.

--- Tests/PrintModelTests/PrintModelTests.swift
+++ Tests/PrintModelTests/PrintModelTests.swift
@@ -1,10 +1,14 @@
 import XCTest
+#if os(macOS)
+import AppKit
+#endif
 @testable import PrintModel
 
 final class PrintModelTests: XCTestCase
-{
-	func testExample() throws
+{	
+	@available(macOS 10.0, *)
+	func testFourCharString()
 	{
-		
+		XCTAssertEqual(NSFileTypeForHFSTypeCode(.init(Int32(fourCharString: "abcd"))), "\'abcd\'")
 	}
 }
